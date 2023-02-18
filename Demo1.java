a3c77b9bfb760fc51f44acb915a376806a0fb1de
Dilshan-Sandaruwan <shydor2005@gmail.com>
2023-02-18 12:04:01 +0530

    first commit

---
+++ Demo1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Demo1{
+	public static void main(String args []){
+		Scanner scan = new Scanner(System.in);
+		int x = 0;
+		while(x<10){
+			System.out.print(x);
+			x=x++;
+			}
+		}
+	}
